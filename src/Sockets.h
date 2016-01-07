2e9ebfad6b26b029f2ad1dec51611814acaefd7b
Aditya Sule <adityagnet@gmail.com>
2016-01-07 12:58:12 -0600

    formatting again

--- src/Sockets.h
+++ src/Sockets.h
@@ -15,53 +15,55 @@
 
 namespace Sockets
 {
-      class Socket
-      {
-            public:
 
-                  /**
-                   * No default constructor. Socket can't be initialised empty!
-                   */
-                  Socket() = delete;
+    class Socket
+    {
 
-                  /**
-                   * Construct Socket from socket file descriptor. File descriptor can be acquired 
-                   * for system other methods. This is okay for RAII because the socket file
-                   * descriptor cannot be acquired from another socket object.
-                   */
-                  Socket(const int &sock_fd);
+        public:
 
-                  /**
-                   * Construct socket from hostname and portnumber/service
-                   */
-                  Socket(const char *host, const char *service);
+            /**
+             * No default constructor. Socket can't be initialised empty!
+             */
+            Socket() = delete;
 
-                  /**
-                   * Default destructor for the Socket class
-                   */
-                  ~Socket();
+            /**
+             * Construct Socket from socket file descriptor. File descriptor can be acquired 
+             * for system other methods. This is okay for RAII because the socket file
+             * descriptor cannot be acquired from another socket object.
+             */
+            Socket(const int &sock_fd);
 
-                  /**
-                   * Binds socket to a given port number. Socket need not be bound for connecting
-                   * to remote host.
-                   */
-                  int bind();
+            /**
+             * Construct socket from hostname and portnumber/service
+             */
+            Socket(const char *host, const char *service);
 
-                  /**
-                   * Socket starts listening for incoming connections. Taks backlog as param.
-                   */
-                  int listen(int &backlog);
-                  int connect();
-                  Socket accept();
-                  int send(char *buff, int &len);
-                  int recv(char *buff, int &len);
+            /**
+             * Default destructor for the Socket class
+             */
+            ~Socket();
 
-            private:
+            /**
+             * Binds socket to a given port number. Socket need not be bound for connecting
+             * to remote host.
+             */
+            int bind();
 
-                  int socket_fd;
-                  struct addrinfo * sa;
-                  decltype(sa->ai_socktype) socktype;
-      };
+            /**
+             * Socket starts listening for incoming connections. Taks backlog as param.
+             */
+            int listen(int &backlog);
+            int connect();
+            Socket accept();
+            int send(char *buff, int &len);
+            int recv(char *buff, int &len);
+
+        private:
+
+            int socket_fd;
+            struct addrinfo * sa;
+            decltype(sa->ai_socktype) socktype;
+    };
 }
 
 #endif
